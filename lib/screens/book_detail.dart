ded0431be3d5790df28635d369b8c0532096d218
이진실 <79292184+ljsil1204@users.noreply.github.com>
2023-06-16 15:09:26 +0900

    도서상세 화면 UI 그리기

--- lib/screens/book_detail.dart
+++ lib/screens/book_detail.dart
@@ -6,12 +6,104 @@ class BookDetail extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("book"),
+        title: Text("book title"),
       ),
-      body: Center(
-          child: Column(
-        children: [Text("hi")],
-      )),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(20)),
+          Image.network(
+            "https://blog.kakaocdn.net/dn/0mySg/btqCUccOGVk/nQ68nZiNKoIEGNJkooELF1/img.jpg",
+            height: 200,
+          ),
+          Padding(padding: EdgeInsets.all(20)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Text(
+                          "다트 배우기",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(
+                        "초보자도 쉽게",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      )
+                    ]),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.15,
+                padding: EdgeInsets.all(10),
+                child: Center(
+                  child: Icon(
+                    Icons.star,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(
+                    Icons.call,
+                    color: Colors.blue,
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    "Contact",
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.near_me,
+                    color: Colors.blue,
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    "Route",
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.save,
+                    color: Colors.blue,
+                  ),
+                  Padding(padding: EdgeInsets.all(5)),
+                  Text(
+                    "Save",
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              )
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(15),
+            child: Text("이책은 쉽고 재밌습니다."),
+          )
+        ],
+      ),
     );
   }
 }
